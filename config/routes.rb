42ce1a5d45e7fcd631c9c511dd47f05370e64d7c
Ivan Gonzalez <ivan@simplelogica.net>
2016-09-17 18:25:04 +0200

    Add new route ‘run’ for case_tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :config_elements
-  resources :case_tests
+  resources :case_tests do
+    get :run
+  end
+
   devise_for :users, only: :session, path: 'session',
              path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users, only: [:show]
