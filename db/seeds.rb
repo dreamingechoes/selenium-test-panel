f064082b962156ce6577b9e83c11ca1f6815d397
Ivan Gonzalez <ivan@simplelogica.net>
2016-09-17 13:27:34 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 User.create!([
-  { password: "123456789", email: "user@example.com" }
+  { password: "123456789", email: "user@example.com", name: "Example user" }
+])
+
+ConfigElement.create!([
+  { key: "browserstack_key" },
+  { key: "browserstack_username" }
 ])
